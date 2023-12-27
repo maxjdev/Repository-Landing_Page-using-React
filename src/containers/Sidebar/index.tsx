451eec46829c42f49c7d0a13c80317f0aa1d5de6
maxjdev <maxjramosdev@gmail.com>
2023-12-27 16:28:48 -0300

    add: about section

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -2,8 +2,8 @@ import Titulo from '../../components/Titulo'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/maxjdev.png" />
-    <Titulo>J Max Gouveia</Titulo>
+    <img src="https://github.com/maxjdev.png" alt='avatar'/>
+    <Titulo fontSize={20}>J Max Gouveia</Titulo>
   </aside>
 )
 
--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,14 +1,20 @@
 import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSection } from './styles'
 
 const Sobre = () => (
   <section>
-    <Titulo>Sobre mim</Titulo>
-    <p>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="principal">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero sint eaque
       nemo laborum praesentium vero voluptatum. Laudantium asperiores
       necessitatibus consequuntur quis, vitae quod iusto nesciunt quam assumenda
       mollitia tempora dolores.
-    </p>
+    </Paragrafo>
+    <GithubSection>
+    <img src="https://github-readme-stats.vercel.app/api?username=maxjdev&show_icons=true&count_private=true&hide_border=true&title_color=00bfbf&icon_color=00bfbf&text_color=c9d1d9&bg_color=0d1117" alt="github stats" /> 
+    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=maxjdev&layout=compact&hide_border=true&title_color=00bfbf&text_color=00bfbf&bg_color=0d1117" alt="github stats" />
+    </GithubSection>
   </section>
 )
 
