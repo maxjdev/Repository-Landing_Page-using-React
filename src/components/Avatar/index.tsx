b48d39cc876f9bbc0016674ed601ac6ce9b4235a
maxjdev <maxjramosdev@gmail.com>
2023-12-27 18:11:59 -0300

    add: style in sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/maxjdev.png" alt="avatar" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/maxjdev.png" alt='avatar'/>
-    <Titulo fontSize={20}>J Max Gouveia</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Max Gouveia</Titulo>
+      <Paragrafo tipo='secundario' fontSize={16}>
+        @maxjdev
+      </Paragrafo>
+      <Descricao tipo='principal' fontSize={12}>
+        Desenvolvedor Full Stack Java
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
